faa5bd371daefaa1d212af972b28ab695180de08
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-07-16 16:51:16 -0300

    DESAFIO QUASE COMPLTO

--- js/carrinho.js
+++ js/carrinho.js
@@ -23,22 +23,60 @@ const addItem = (objItem) => {
   const posicao = itensCarrinho.findIndex(elem => elem.id_produto === objItem.id_produto)
 
   if (posicao === -1) {
-  itensCarrinho.push(item(objItem))
+    itensCarrinho.push(item(objItem))
   } else {
     itensCarrinho[posicao].quantidade++
-}
+  }
 
 
   sessionStorage.setItem('carrinhoSessao', JSON.stringify
     (itensCarrinho))
 
 }
-
 const listItens = () => {
-  const listaItens = JSON.parse(sessionStorage.getItem('carrinhoSessao'))
+  const listaItens =
+      JSON.parse(sessionStorage.getItem("carrinhoSessao")) || [];
 
-  return listaItens
+  return listaItens;
 }
+
+// TENTANDO CRIAR A FUNÇAO DE SOMA TOTAL
+const somaTotal = () => {
+
+  let soma = 0;
+
+  listItens().forEach((elem) => {
+      soma += elem.valor_unitario * elem.quantidade;
+  });
+  sessionStorage.setItem('carrinhoSessao', JSON.stringify
+  (itensCarrinho))
+  
+  return soma;
+};
+const valorPagar = () =>{
+  let frete =`R$${(10).toFixed(2).replace('.', ',')}`;
+  let pagar = 0;
+   
+  listItens().forEach((elem) => {
+    pagar += soma + frete;
+});
+sessionStorage.setItem('carrinhoSessao', JSON.stringify
+(itensCarrinho))
+
+return pagar
+}
+
+const alterarQuantidade = (pos, quantidade) => {
+
+itensCarrinho[pos].quantidade = quantidade;
+
+sessionStorage.setItem(
+    'carrinhoSessao',
+    JSON.stringify(itensCarrinho)
+);
+
+}
+  
 
 // FUNÇÃO REMOVER ITEM DO ARRAY
 const removeItem = (pos) => {
@@ -46,6 +84,9 @@ const removeItem = (pos) => {
 
   sessionStorage.setItem('carrinhoSessao', JSON.stringify
     (itensCarrinho))
-}
 
-export { addItem, listItens, removeItem }
+  }
+
+
+ 
+export { addItem, listItens, removeItem , somaTotal, alterarQuantidade , valorPagar}
